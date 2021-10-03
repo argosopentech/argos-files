efe96702e3fac466388d9e0370e1d5eb56b8c5f5
P.J. Finlay <pjappdevelopment@gmail.com>
2021-10-03 14:41:05 -0500

    Install Apache

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-sudo snap install ipfs
+# Runs as root
+
+# Install IPFS
+snap install ipfs
 ipfs init
 ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme
+
+# Install Apache
+apt-get install -y apache2
+
