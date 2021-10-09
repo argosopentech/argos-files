a2a34f01d934c6435d52f93c470a0f7142f83eac
P.J. Finlay <pjappdevelopment@gmail.com>
2021-10-09 12:45:30 +0000

    Downloading files from index

--- download-index.py
+++ download-index.py
@@ -2,14 +2,12 @@
 
 import argparse
 import json
-
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('index')
-parser.add_argument('data-dir')
+parser.add_argument('data_dir')
 args = parser.parse_args()
-
-
 
 with open(args.index) as index_file:
     index = json.load(index_file)
@@ -17,6 +15,7 @@ with open(args.index) as index_file:
         links = metadata.get('links')
         if not links: continue
         for link in links:
-            print(link)
-
+            if link[0:4] != 'http': continue
+            print(f'Downloading {link}')
+            subprocess.run(['wget', '--directory-prefix', args.data_dir, link])
 
--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme
 # Install Apache
 apt-get install -y apache2
 
+# Install dependencies
+apt-get install -y wget python3 vim git
+
 # Make sym link to server dir
 ln -s /var/www/html ~/data
 
