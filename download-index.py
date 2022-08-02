508baf1ea68b0f23b8d056b52d0d6531781907a3
P.J. Finlay <pjappdevelopment@gmail.com>
2022-08-01 20:04:25 -0500

    Update download-index.py

--- download-index.py
+++ download-index.py
@@ -12,10 +12,7 @@ args = parser.parse_args()
 with open(args.index) as index_file:
     index = json.load(index_file)
     for metadata in index:
-        links = metadata.get('links')
-        if not links: continue
-        for link in links:
-            if link[0:4] != 'http': continue
-            print(f'Downloading {link}')
-            subprocess.run(['wget', '--directory-prefix', args.data_dir, link])
+        link = metadata.get('links')[0]
+        print(f'Downloading {link}')
+        subprocess.run(['wget', '--directory-prefix', args.data_dir, link])
 
