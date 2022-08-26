f99938639e428580a126e824f2ade6ffb1486bdc
P.J. Finlay <pjappdevelopment@gmail.com>
2022-08-26 17:30:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme
 apt-get install -y apache2
 
 # Install dependencies
-apt-get install -y wget python3 vim git
+apt-get install -y wget python3 python-is-python3 vim git
 
 # Make sym link to server dir
 ln -s /var/www/html ~/data
